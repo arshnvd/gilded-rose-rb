0c6cc51c9aa4b189e7345919f9dc889456d067c5
Arash Naveed <arsh.nvd@gmail.com>
2020-01-19 00:33:41 +0500

    Update test cases

--- gilded_rose_tests.rb
+++ gilded_rose_tests.rb
@@ -5,7 +5,7 @@ require 'test/unit'
 
 class TestGildedRose < Test::Unit::TestCase
   def test_normal_item_before_sell_date
-    item        = Item.new('Normal Item', 10, 5)
+    item        = GildedRose.item('Normal Item', 10, 5)
     gilded_rose = GildedRose.new([item])
     gilded_rose.update_quality
     assert_equal item.name, 'Normal Item'
@@ -13,21 +13,21 @@ class TestGildedRose < Test::Unit::TestCase
   end
 
   def test_normal_item_on_sell_date
-    item        = Item.new('Normal Item', 0, 4)
+    item        = GildedRose.item('Normal Item', 0, 4)
     gilded_rose = GildedRose.new([item])
     gilded_rose.update_quality
     assert_equal item.quality, 2
   end
 
   def test_normal_item_after_sell_date
-    item        = Item.new('Normal Item', -7, 45)
+    item        = GildedRose.item('Normal Item', -7, 45)
     gilded_rose = GildedRose.new([item])
     gilded_rose.update_quality
     assert_equal item.quality, 43
   end
 
   def test_aged_brie_item_before_sell_date
-    item        = Item.new('Aged Brie', 10, 22)
+    item        = GildedRose.item('Aged Brie', 10, 22)
     gilded_rose = GildedRose.new([item])
     gilded_rose.update_quality
     assert_equal item.name, 'Aged Brie'
@@ -35,7 +35,7 @@ class TestGildedRose < Test::Unit::TestCase
   end
 
   def test_aged_brie_item_on_sell_date
-    item        = Item.new('Aged Brie', 0, 44)
+    item        = GildedRose.item('Aged Brie', 0, 44)
     gilded_rose = GildedRose.new([item])
     gilded_rose.update_quality
     assert_equal item.name, 'Aged Brie'
@@ -43,14 +43,14 @@ class TestGildedRose < Test::Unit::TestCase
   end
 
   def test_aged_brie_item_after_sell_date
-    item        = Item.new('Aged Brie', -4, 22)
+    item        = GildedRose.item('Aged Brie', -4, 22)
     gilded_rose = GildedRose.new([item])
     gilded_rose.update_quality
     assert_equal item.quality, 24
   end
 
   def test_sulfuras_item_before_sell_date
-    item        = Item.new('Sulfuras, Hand of Ragnaros', 10, 20)
+    item        = GildedRose.item('Sulfuras, Hand of Ragnaros', 10, 20)
     gilded_rose = GildedRose.new([item])
     gilded_rose.update_quality
     assert_equal item.name, 'Sulfuras, Hand of Ragnaros'
@@ -58,7 +58,7 @@ class TestGildedRose < Test::Unit::TestCase
   end
 
   def test_sulfuras_item_on_sell_date
-    item        = Item.new('Sulfuras, Hand of Ragnaros', 0, 20)
+    item        = GildedRose.item('Sulfuras, Hand of Ragnaros', 0, 20)
     gilded_rose = GildedRose.new([item])
     gilded_rose.update_quality
     assert_equal item.name, 'Sulfuras, Hand of Ragnaros'
@@ -66,14 +66,14 @@ class TestGildedRose < Test::Unit::TestCase
   end
 
   def test_sulfuras_item_after_sell_date
-    item        = Item.new('Sulfuras, Hand of Ragnaros', -5, 20)
+    item        = GildedRose.item('Sulfuras, Hand of Ragnaros', -5, 20)
     gilded_rose = GildedRose.new([item])
     gilded_rose.update_quality
     assert_equal item.quality, 20
   end
 
   def test_backstage_item_15_days_before_sell_date
-    item        = Item.new('Backstage passes to a TAFKAL80ETC concert', 15, 10)
+    item        = GildedRose.item('Backstage passes to a TAFKAL80ETC concert', 15,10)
     gilded_rose = GildedRose.new([item])
     gilded_rose.update_quality
     assert_equal item.name, 'Backstage passes to a TAFKAL80ETC concert'
@@ -81,7 +81,7 @@ class TestGildedRose < Test::Unit::TestCase
   end
 
   def test_backstage_item_10_days_before_sell_date
-    item        = Item.new('Backstage passes to a TAFKAL80ETC concert', 10, 44)
+    item        = GildedRose.item('Backstage passes to a TAFKAL80ETC concert', 10,44)
     gilded_rose = GildedRose.new([item])
     gilded_rose.update_quality
     assert_equal item.name, 'Backstage passes to a TAFKAL80ETC concert'
@@ -89,7 +89,7 @@ class TestGildedRose < Test::Unit::TestCase
   end
 
   def test_backstage_item_5_days_before_sell_date
-    item        = Item.new('Backstage passes to a TAFKAL80ETC concert', 5, 30)
+    item        = GildedRose.item('Backstage passes to a TAFKAL80ETC concert', 5,30)
     gilded_rose = GildedRose.new([item])
     gilded_rose.update_quality
     assert_equal item.name, 'Backstage passes to a TAFKAL80ETC concert'
@@ -97,7 +97,7 @@ class TestGildedRose < Test::Unit::TestCase
   end
 
   def test_backstage_item_on_sell_date
-    item        = Item.new('Backstage passes to a TAFKAL80ETC concert', 0, 20)
+    item        = GildedRose.item('Backstage passes to a TAFKAL80ETC concert', 0,20)
     gilded_rose = GildedRose.new([item])
     gilded_rose.update_quality
     assert_equal item.name, 'Backstage passes to a TAFKAL80ETC concert'
@@ -105,7 +105,7 @@ class TestGildedRose < Test::Unit::TestCase
   end
 
   def test_backstage_item_after_sell_date
-    item        = Item.new('Backstage passes to a TAFKAL80ETC concert', -3, 20)
+    item        = GildedRose.item('Backstage passes to a TAFKAL80ETC concert', -3,20)
     gilded_rose = GildedRose.new([item])
     gilded_rose.update_quality
     assert_equal item.quality, 0
